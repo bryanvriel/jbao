883d346ebccfce02b4d7e0a3dea152848d950fb8
Bryan Riel <bryanvriel@gmail.com>
2020-09-14 10:39:58 +0800

    Adding network for spatial inputs.

--- jbao/gradients.py
+++ jbao/gradients.py
@@ -3,14 +3,15 @@
 import jax
 import jax.numpy as jnp
 
-def vgrad(func, *args, argnum=1):
+def vgrad(func, *args, n_inputs=1, argnum=1):
     """
     Convenience function for calling jax.vmap for a jax.grad computation in order to
     compute gradients for a batch of examples.
     """
     # Construct the in_axes tuple
-    n_array_inputs = len(args) - 1
-    in_axes = (None,) + (0,) * n_array_inputs
+    in_axes = (None,) * (len(args) - n_inputs) + (0,) * n_inputs
+    #n_array_inputs = len(args) - 1
+    #in_axes = (None,) + (0,) * n_array_inputs
 
     # Construct the vmap command
     vmapped_grad = jax.vmap(jax.grad(func, argnum), in_axes=in_axes, out_axes=0)
--- jbao/networks.py
+++ jbao/networks.py
@@ -22,7 +22,7 @@ class Model:
 
     def __call__(self, params, *args):
         return self.net.apply(params[self.name], *args)
-
+        
 
 class ModelCollection:
 
@@ -84,7 +84,7 @@ class _DenseNetwork(hk.Module):
         # Save activation function
         self.actfun = actfun
 
-    def __call__(self, X):
+    def __call__(self, *args):
 
         # Create model data
         data = []
@@ -96,8 +96,14 @@ class _DenseNetwork(hk.Module):
         # Create the sequential model
         model = hk.Sequential(data)
 
-        # Run prediction
-        return model(X)
+        # Optionally concatenate arguments
+        if len(args) > 1:
+            cargs = jnp.column_stack(args)
+        else:
+            cargs = args
+
+        # Run prediction and squeeze outputs (for jax.grad compatibility)
+        return jnp.squeeze(model(cargs))
 
 
 class DenseNetwork(Model):
@@ -105,7 +111,8 @@ class DenseNetwork(Model):
     Higher level class for dense network.
     """
 
-    def __init__(self, input_dim, output_dim, *args, name='model'):
+    def __init__(self, n_layers, hidden_units, input_dim=1, output_dim=1, n_inputs=1,
+                 name='model', **kwargs):
 
         # Initialize base model class
         super().__init__(name=name)
@@ -113,22 +120,93 @@ class DenseNetwork(Model):
         # Store the input and output dimension sizes
         self.input_dim = input_dim
         self.output_dim = output_dim
+        self.n_inputs = n_inputs
 
         # Transform the network function
         self.net = hk.without_apply_rng(hk.transform(
-            lambda *dargs: _DenseNetwork(output_dim, *args, name=name)(*dargs)
+            lambda *dargs: _DenseNetwork(output_dim, n_layers, hidden_units,
+                                         name=name)(*dargs)
         ))
 
     def init_params(self, rng):
         """
         Randomly initialize parameters.
         """
-        # Make inputs with right shape for dense network
-        inputs = jnp.zeros((1, self.input_dim), dtype=jnp.float32)
+        # Make inputs with correct shapes
+        if self.n_inputs > 1:
 
-        # Call initialization
-        params = self.net.init(rng, inputs)
+            # Make list of inputs
+            inputs = []
+            for i in range(self.n_inputs):
+                inputs.append(jnp.zeros((1, self.input_dim), dtype=jnp.float32))
+
+            # Send to network init
+            params = self.net.init(rng, *inputs)
+
+        # Otherwise, send in a single input
+        else:
+            inputs = jnp.zeros((1, self.input_dim), dtype=jnp.float32)
+            params = self.net.init(rng, inputs)
+
         return params
 
+
+class SpatialNetwork(Model):
+    """
+    Specialized higher level dense network for taking in multiple spatial
+    coordinates as inputs.
+    """
+
+    def __init__(self, n_layers, hidden_units, input_dim=1, output_dim=1,
+                 name='model', **kwargs):
+
+        # Initialize base model class
+        super().__init__(name=name)
+
+        # Store the input and output dimension sizes
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.n_inputs = 2
+
+        # Transform the network function
+        self.net = hk.without_apply_rng(hk.transform(
+            lambda *dargs: _DenseNetwork(output_dim, n_layers, hidden_units,
+                                         name=name)(*dargs)
+        ))
+
+    def init_params(self, rng):
+        """
+        Randomly initialize parameters.
+        """
+        # Make list of inputs
+        inputs = []
+        for i in range(self.n_inputs):
+            inputs.append(jnp.zeros((1, self.input_dim), dtype=jnp.float32))
+
+        # Send to network init
+        return self.net.init(rng, *inputs)
+
+    #def dx(self, params, X, Y):
+    #    dx = 1.0e-5
+    #    fwd = self.net.apply(params[self.name], X + dx, Y)
+    #    bwd = self.net.apply(params[self.name], X - dx, Y)
+    #    return (fwd - bwd) / (2.0 * dx)
+
+    #def dy(self, params, X, Y):
+    #    dy = 1.0e-5
+    #    fwd = self.net.apply(params[self.name], X, Y + dy)
+    #    bwd = self.net.apply(params[self.name], X, Y - dy)
+    #    return (fwd - bwd) / (2.0 * dy)
+
+    def dx(self, params, X, Y):
+        gradfun = jax.vmap(jax.grad(self.net.apply, 1), in_axes=(None, 0, 0), out_axes=0)
+        return jnp.squeeze(gradfun(params[self.name], X, Y))
+
+    def dy(self, params, X, Y):
+        gradfun = jax.vmap(jax.grad(self.net.apply, 2), in_axes=(None, 0, 0), out_axes=0)
+        return jnp.squeeze(gradfun(params[self.name], X, Y))
+
+
+        
 
 # end of file
